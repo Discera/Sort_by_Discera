24aa35604f4d200f1bb4b866e53b71143663c130
Discera <90552528+Discera@users.noreply.github.com>
2022-07-18 15:43:43 +0200

    Beautify BubblesortReverse.java

--- BubblesortReverse.java
+++ BubblesortReverse.java
@@ -1,25 +1,24 @@
 package AllTogether;
 
 public class BubblesortReverse {
-	   static int[] sort(int[] list){
-	        sort(list, 0,list.length-1);
-	        return list;
-	    }
-	    public static void sort(int[] list, int low, int max) {
+  static int[] sort(int[] list) {
+    sort(list, 0, list.length - 1);
+    return list;
+  }
+  public static void sort(int[] list, int low, int max) {
 
+    for (int i = max; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
 
-	        for (int i = max; i > 0; i--) {
-	            for (int j = 0; j < i; j++) {
-
-	                if (list[j] < list[j + 1]) {
-	                    int temp = list[j];
-	                    list[j] = list[j + 1];
-	                    list[j + 1] = temp;
-	                }
-	            }
-	        }
-	        for (int i = 0; i < list.length; i++) {
-	            System.out.println(list[i]);
-	        }
-	    }
+        if (list[j] < list[j + 1]) {
+          int temp = list[j];
+          list[j] = list[j + 1];
+          list[j + 1] = temp;
+        }
+      }
+    }
+    for (int i = 0; i < list.length; i++) {
+      System.out.println(list[i]);
+    }
+  }
 }
