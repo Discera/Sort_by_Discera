060005ebc369c9ae53317224f569489a514b4257
Discera <90552528+Discera@users.noreply.github.com>
2022-07-18 15:44:19 +0200

    Beautify MainSort.java

--- MainSort.java
+++ MainSort.java
@@ -4,19 +4,17 @@ import java.awt.EventQueue;
 
 public class MainSort {
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SortJFrame frame = new SortJFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		
-		
-	}
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          SortJFrame frame = new SortJFrame();
+          frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+  }
 }
